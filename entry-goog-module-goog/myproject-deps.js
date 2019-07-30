30966a71d0495f8b5a20c4a9489d801e8daddef4
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-07-30 11:26:08 +0900

    rename: entry-goog-module-goog

---
+++ entry-goog-module-goog/myproject-deps.js
@@ -0,0 +1,4 @@
+goog.addDependency('../../../../entry-goog-module-goog/bar.js', ['myproject.bar'], ['goog.string'], {'lang': 'es6'});
+goog.addDependency('../../../../entry-goog-module-goog/esmodule.js', [], ['goog.math'], {'lang': 'es6', 'module': 'es6'});
+goog.addDependency('../../../../entry-goog-module-goog/foo.js', ['myproject.foo'], ['goog.array', 'goog.dom', 'myproject.bar'], {'lang': 'es6'});
+
--- myproject/myproject-deps.js
+++
@@ -1,3 +0,0 @@
-goog.addDependency('../../../../myproject/bar.js', ['myproject.bar'], ['goog.string'], {'lang': 'es6'});
-goog.addDependency('../../../../myproject/foo.js', ['myproject.foo'], ['goog.array', 'goog.dom', 'myproject.bar'], {'lang': 'es6'});
-
