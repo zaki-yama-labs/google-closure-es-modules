0f089900af4dd2e4ac9e570ca5e01669d95e41e5
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-07-30 11:19:14 +0900

    entry: es, module: es

---
+++ entry-es-module-es/esmodule.js
@@ -0,0 +1,3 @@
+const math = goog.require('goog.math');
+
+export const CONSTANT = math.sum(1, 1, 2, 3, 5, 8, 13, 21);
---
+++ entry-es-module-es/myproject-deps.js
@@ -0,0 +1,3 @@
+goog.addDependency('../../../../entry-es-module-es/esmodule.js', [], ['goog.math'], {'lang': 'es6', 'module': 'es6'});
+goog.addDependency('../../../../entry-es-module-es/foo.js', ['myproject.foo'], ['../../../../entry-es-module-es/esmodule.js'], {'lang': 'es6', 'module': 'es6'});
+
