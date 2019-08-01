0e889efe0d48ca4207ebdf60a35771671389cf83
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-08-01 10:04:44 +0900

    entry: goog.module, module: es

---
+++ entry-goog-modules-module-es/entry.js
@@ -0,0 +1,4 @@
+goog.module('myproject.main');
+
+const { Es6Class } = goog.require('myproject.es6module');
+new Es6Class().render();
---
+++ entry-goog-modules-module-es/myproject-deps.js
@@ -0,0 +1,3 @@
+goog.addDependency('../../../../entry-goog-modules-module-es/entry.js', ['myproject.main'], ['myproject.es6module'], {'lang': 'es6', 'module': 'goog'});
+goog.addDependency('../../../../entry-goog-modules-module-es/es6module.js', ['myproject.es6module'], [], {'lang': 'es6', 'module': 'es6'});
+
